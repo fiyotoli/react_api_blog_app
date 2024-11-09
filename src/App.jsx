2cec6dfbbb78e08b4fc883cf4fbebf574ab5e603
fiyotoli <tasfayneshtolasa35@gmail.com>
2024-11-09 10:23:47 +0300

    final commit

--- src/App.jsx
+++ src/App.jsx
@@ -24,7 +24,7 @@ const App = () => {
   }, []);
 
   return (
-    <Router>
+    <Router  basename="/react_api_blog_app">
       <Navbar />
 
       {/* Conditional rendering for loading spinner */}
