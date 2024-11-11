d7bd2eb385f1748d6f1ff0e26784e1c0400ecddf
fiyotoli <tasfayneshtolasa35@gmail.com>
2024-11-11 12:11:49 +0300

    last commit

--- src/App.jsx
+++ src/App.jsx
@@ -18,7 +18,7 @@ const App = () => {
   useEffect(() => {
     const timer = setTimeout(() => {
       setLoading(false);
-    }, 1000); // Spinner will be visible for 3 seconds
+    }, 1100); // Spinner will be visible for 3 seconds
 
     return () => clearTimeout(timer); // Cleanup the timer when the component unmounts
   }, []);
