6f46f73447122e03781619f9d4e193b7e30a0907
fiyotoli <tasfayneshtolasa35@gmail.com>
2024-11-11 09:40:53 +0300

    final commit

--- vite.config.js
+++ vite.config.js
@@ -4,5 +4,8 @@ import react from '@vitejs/plugin-react'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: '/react_api_blog_app/',
+  base: '/react_api_blog_app/', // Update to match your GitHub repository name
+  build: {
+    chunkSizeWarningLimit: 1000, // Increase the chunk size warning limit (in KB)
+  },
 })
